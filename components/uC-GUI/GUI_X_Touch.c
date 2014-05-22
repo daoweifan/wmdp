5c0e9d50f80b7822c100e6c2916601229f2f00b7
unknown <zz1tlk@DLGZH36S1.Asia.DelphiAuto.net>
2014-05-22 19:56:03 +0800

    GUI Touch, add calibration function, should be called in task init section
    Signed-off-by: unknown <zz1tlk@DLGZH36S1.Asia.DelphiAuto.net>

--- components/uC-GUI/GUI_X_Touch.c
+++ components/uC-GUI/GUI_X_Touch.c
@@ -21,7 +21,9 @@ Purpose     : Config / System dependent externals for GUI
 #include "GUI_X.h"
 #include "device.h"
 #include "os.h"
-#include <stdio.h>
+#include "gui.h"
+#include "GUITouchConf.h"
+#include "LCDConf.h"
 
 extern struct device * gui_touch;
 
@@ -56,3 +58,52 @@ void GUI_BG_Callback(void)
 	GUI_TOUCH_Exec();
 }
 EXPORT_OS_BG_16MS_UPDATE(GUI_BG_Callback)
+
+#define LCD_CROSS_X1 (50)
+#define LCD_CROSS_X2 (LCD_XSIZE - 50)
+#define LCD_CROSS_X3 (50)
+#define LCD_CROSS_X4 (LCD_XSIZE - 50)
+#define LCD_CROSS_Y1 (50)
+#define LCD_CROSS_Y2 (50)
+#define LCD_CROSS_Y3 (LCD_YSIZE - 50)
+#define LCD_CROSS_Y4 (LCD_YSIZE - 50)
+
+static void LCD_PUTCROSS(int x, int y)
+{
+	GUI_DrawHLine(y, x-5, x+5);
+	GUI_DrawVLine(x, y-5, y+5);
+}
+
+/* calibrate the touch screen */
+void GUI_TOUCH_X_Calibration(void)
+{
+	int result;
+	uint16_t x1position, y1position;
+	uint16_t x4position, y4position;
+
+	do {
+		GUI_Clear();
+		/* first point */
+		LCD_PUTCROSS(LCD_CROSS_X1, LCD_CROSS_Y1);
+		do {
+			device_control(gui_touch, TOUCH_GET_X_POSITION, &x1position);
+			device_control(gui_touch, TOUCH_GET_Y_POSITION, &y1position);
+		} while(!((x1position > 0x00A0) && (x1position < 0x0738) && (y1position > 0x0118) && (y1position < 0x06BB)));
+
+		/* forth point */
+		udelay(5000000);
+		LCD_PUTCROSS(LCD_CROSS_X4, LCD_CROSS_Y4);
+		GUI_DrawLine(LCD_CROSS_X1,LCD_CROSS_Y1, LCD_CROSS_X4, LCD_CROSS_Y4);
+		do {
+			device_control(gui_touch, TOUCH_GET_X_POSITION, &x4position);
+			device_control(gui_touch, TOUCH_GET_Y_POSITION, &y4position);
+		} while(!((x4position > 0x00A0) && (x4position < 0x0738) && (y4position > 0x0118) && (y4position < 0x06BB)));
+
+		/* calibration */
+		result = 0;
+		result = GUI_TOUCH_Calibrate(0, LCD_CROSS_X1, LCD_CROSS_X4, x1position, x4position);
+		result += GUI_TOUCH_Calibrate(1, LCD_CROSS_Y1, LCD_CROSS_Y4, y1position, y4position);
+	} while (result);
+}
+
+EXPORT_TASK_INIT(GUI_TOUCH_X_Calibration)
